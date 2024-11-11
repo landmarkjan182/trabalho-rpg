e97dbd45208921011fac672978354a33b674e1c1
joao95352 <victorjoao95504@gmail.com>
2024-11-11 08:54:21 -0300

    correcao de bug

--- main/bau.py
+++ main/bau.py
@@ -2,20 +2,21 @@ from escrever_devagar import escrever_devagar
 import time
 from random import randint
 from combate import combate
-import abrir_bau
+from abrir_bau import abrir_bau
+from criar_personagem import criar_personagem
 def desafio_bau(personagem):
 
     escrever_devagar("\n🗺️ Você está explorando... procurando por tesouros... 🗺️")
     time.sleep(1)
     desafio = randint(1, 20)
 
-    if desafio <= 4:
+    if desafio <= 20:
         escrever_devagar("\n🎉 Você encontrou um baú brilhante! 🎉\n")
         time.sleep(1)
         escrever_devagar("Abrindo o baú...")
         time.sleep(1)
 
-        if randint(1, 10) == 1: 
+        if randint(1, 1) == 1: 
             escrever_devagar("⚠️ Mas espere... algo está errado... ⚠️")
             time.sleep(1)
             escrever_devagar("\n😈 O baú se transformou em um Mimético! 😈\n")
